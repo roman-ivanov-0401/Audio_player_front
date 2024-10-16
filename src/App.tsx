e666164b81d8c2b9c1e16b411dedda9ee8abbf79
romanivanov0401 <romanivanov0401@gmail.com>
2024-10-16 06:00:42 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+	return <>Hello music!</>;
+}
+
+export default App;
---
+++ src/providers/RouterProvider.tsx
@@ -0,0 +1,5 @@
+import { FC, PropsWithChildren } from "react";
+
+export const RouterProvider: FC<PropsWithChildren> = ({ children  }) => {
+    return children
+}
